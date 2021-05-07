28a58581402169b784a30e041cd8dfa3c3a2a312
SakurabaKiyoka <sakurabakiyoka@yahoo.co.jp>
2021-05-07 20:44:05 +0900

    fix code bug

--- js/sakura-app.js
+++ js/sakura-app.js
@@ -1120,11 +1120,11 @@ var home = location.href,
             locale: 'zh',
             show_email_subscription: true 
             })
+            !function(e,n){for(var o=0;o<e.length;o++){var r=n.createElement("script"),c=".js",d=n.head||n.body;"noModule"in r?(r.type="module",c=".mjs"):r.async=!0,r.defer=!0,r.src=remark_config.host+"/web/"+e[o]+c,d.appendChild(r)}}(remark_config.components||["embed"],document)
       }
         }catch(e){
             
         }
-    !function(e,n){for(var o=0;o<e.length;o++){var r=n.createElement("script"),c=".js",d=n.head||n.body;"noModule"in r?(r.type="module",c=".mjs"):r.async=!0,r.defer=!0,r.src=remark_config.host+"/web/"+e[o]+c,d.appendChild(r)}}(remark_config.components||["embed"],document)
     },
     MJ: function () {
       if (mashiro_option.mathjax == '1') {
